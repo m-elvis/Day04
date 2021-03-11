c838ce5ef07ddbdf76672ba76bde163ba92c289c
m-elvis <elvis.mahovic@gmail.com>
2021-03-11 23:40:29 +0100

    name,lastname

---
+++ ex10/src/index.js
@@ -0,0 +1,14 @@
+function main(){
+    var myFirstName = "Elvis";
+    var myLastName = "Mahovic";
+    var myHomeTown = "Sarajevo";
+          
+    return {
+    myFirstName,
+    myLastName,
+    myHomeTown
+    };
+    
+    }
+    console.log(main());
+    module.exports = main;
